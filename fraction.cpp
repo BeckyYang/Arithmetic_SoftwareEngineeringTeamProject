ab559c0fa665a99e5d2b314dba83bd70557267ea
BeckyYang <45991523+BeckyYang@users.noreply.github.com>
2019-01-13 00:29:56 +0800

    fraction.cpp第二版

--- fraction.cpp
+++ fraction.cpp
@@ -1,13 +1,13 @@
+/*
+----------------------------------
+谢蜜雪 学号：1120161761
+日期：2019/1/12
+实现功能：分数的类fraction的源文件
+----------------------------------
+*/
 #pragma once
 #include "stdafx.h"
 #include "fraction.h"
-
-fraction& fraction:: operator = (fraction a)
-{
-	numerator = a.numerator;
-	denominator = a.denominator;
-	return *this;
-}
 
 fraction::fraction(int x, int y)
 {
@@ -23,4 +23,59 @@ int fraction::GetNumerator()
 int fraction::GetDenominator()
 {
 	return denominator;
+}
+
+int FracionGetGCD(int a, int b)
+{
+	return (a % b == 0) ? b : FracionGetGCD(b, a%b);
+}
+
+fraction& operator +(fraction& A, fraction &B)
+{
+	int x, y, z;
+	x = A.numerator * B.denominator + A.denominator * B.numerator;
+	y = A.denominator * B.denominator;
+	z = FracionGetGCD(x, y);
+	fraction f(x / z, y / z);
+	return f;
+}
+
+fraction& operator -(fraction &A, fraction &B)
+{
+	int x, y, z;
+	x = A.numerator * B.denominator - A.denominator * B.numerator;
+	y = A.denominator * B.denominator;
+	z = FracionGetGCD(x, y);
+	fraction f(x / z, y / z);
+	return f;
+}
+
+fraction& operator *(fraction &A, fraction &B)
+{
+	int x, y, z;
+	x = A.numerator * B.numerator;
+	y = A.denominator * B.denominator;
+	z = FracionGetGCD(x, y);
+	fraction f(x / z, y / z);
+	return f;
+}
+
+fraction& operator /(fraction &A, fraction &B)
+{
+	int x, y, z;
+	x = A.numerator * B.denominator;
+	y = A.denominator * B.numerator;
+	z = FracionGetGCD(x, y);
+	fraction f(x / z, y / z);
+	return f;
+}
+
+int operator <(fraction&A, fraction &B)
+{
+	int x, y, z;
+	x = A.numerator * B.denominator - A.denominator * B.numerator;
+	y = A.denominator * B.denominator;
+	z = (x > 0 && y < 0) || (x < 0 && y > 0);
+	return z;
+
 }
