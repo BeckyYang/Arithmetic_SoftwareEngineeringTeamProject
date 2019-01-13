b42baad4fd381fd7b8162e85c0163a316c54161d
BeckyYang <45991523+BeckyYang@users.noreply.github.com>
2019-01-13 11:54:28 +0800

    主函数（合并版）

--- Arithmetic_Main.cpp
+++ Arithmetic_Main.cpp
@@ -2,7 +2,7 @@
 ----------------------------------------------------
 谢蜜雪 学号：1120161761
 杨冰琪 学号：1120161762
-日期：2019/1/12
+日期：2019/1/13
 实现功能：生成N个四则运算题目和求解题目的主函数入口
 ---------------------------------------------------
 */
@@ -22,10 +22,12 @@ file_path为存放题目的文件地址
 #pragma once
 
 #include "stdafx.h"
-//#include "fraction.h"
+#include "Calculate.h"
 #include "PuzzleGenerate.h"
 #include <ctime>
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <algorithm>
 
 using namespace std;
@@ -38,17 +40,19 @@ int main(int argc, char * argv[])
 	start_time = clock();
 
 	int N = 0; //题目数量
+	string Question, userAnswer;;
+	ifstream inf;
 
 	if (argc == 4 && strcmp(argv[1], "-p") == 0) //生成运算题目命令
 	{
 		if (strlen(argv[3]) != 1) //题目类型type长度不合法
 		{
-			cout << "命令行格式有误！";
+			cout << "命令行格式有误！" << endl;
 			return 0;
 		}
 		if (argv[3][0] != 'A' && argv[3][0] != 'B' && argv[3][0] != 'C') //题目类型type包含非法字符
 		{
-			cout << "命令行格式有误！";
+			cout << "命令行格式有误！" << endl;
 			return 0;
 		}
 		int len = strlen(argv[2]);
@@ -57,7 +61,7 @@ int main(int argc, char * argv[])
 		{
 			if (argv[2][L] < '0' || argv[2][L] > '9') //题目数量N包含非法字符
 			{
-				cout << "命令行格式有误！";
+				cout << "命令行格式有误！" << endl;
 				return 0;
 			}
 			N = N * 10 + argv[2][L] - '0';
@@ -65,19 +69,38 @@ int main(int argc, char * argv[])
 		}
 		if (N < 1 || N > 1000) //题目数量N超出范围
 		{
-			cout << "命令行格式有误！";
+			cout << "命令行格式有误！" << endl;
 			return 0;
 		}
 		PuzzleGenerate(argv[0], N, argv[3][0]);
 	}
 
-	else if (strcmp(argv[1], "-a") == 0) //求解题目命令
+	else if (argc == 3 && strcmp(argv[1], "-a") == 0) //求解题目命令
 	{
-
+		string Filepath = argv[2];
+		inf.open(Filepath);
+		if (inf.fail() == true) 
+		{
+			cout << "Fail to open file!" << endl;
+		}
+		int count = 0;//题号
+		while (getline(inf, Question))
+		{
+			cout << "No." << count++ << ": " << Question << endl;
+			cout << "Your answer:";
+			cin >> userAnswer;
+			Calculate(Question);
+			UserGUI(userAnswer);
+		}
 	}
-
+	else
+	{
+		cout << "命令行格式有误！"<<endl;
+		return 0;
+	}
 	finish_time = clock();
 	totaltime = ((double)(finish_time)-(double)start_time) / CLOCKS_PER_SEC;
 	cout << "程序的运行时间为" << totaltime << "秒！" << endl;
+	system("pause");
 	return 0;
 }
